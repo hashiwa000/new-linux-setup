4bf2c3b49bb90acaa718e2a1702767180fd88de2
hashiwa000 <hashiwa000>
2025-10-21 21:53:18 +0900

    update

--- upgrade.sh
+++ upgrade.sh
@@ -14,10 +14,22 @@ echo
 echo "##### sudo apt autoremove -y"
 sudo apt autoremove -y
 
-echo
-echo "##### uv self update"
-uv self update
+command -v uv > /dev/null
+uv_installed=$?
+if [ ${uv_installed} -eq 0 ] ; then
+  echo
+  echo "##### uv self update"
+  uv self update
 
-echo
-echo "##### npm update"
-npm update
+  echo
+  echo "##### uv tool upgrade --all"
+  uv tool upgrade --all
+fi
+
+command -v npm > /dev/null
+npm_installed=$?
+if [ ${npm_installed} -eq 0 ] ; then
+  echo
+  echo "##### npm update"
+  npm update
+fi
